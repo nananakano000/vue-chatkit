4568d1ef0f3ac50ae2d1f39477c33c0cb4b2c375
nakanoh <murry.xxx.7@gmail.com>
2020-03-17 17:25:41 +0900

    〜ルームへの加入（LoginForm.vueにバグあり、次回対応）

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,6 +9,7 @@ function handleError(commit, error) {
 export default {
   async login({ commit, state }, userId) {
     try {
+      console.log("hogehoge")
       commit('setError', '');
       commit('setLoading', true);
       // Connect user to ChatKit service
@@ -21,6 +22,25 @@ export default {
 
       // Test state.user
       console.log(state.user);
+
+      //... (place right after the `setUser` commit statement)
+      // Save list of user's rooms in store
+      const rooms = currentUser.rooms.map(room => ({
+        id: room.id,
+        name: room.name
+      }))
+      commit('setRooms', rooms);
+
+      // Subscribe user to a room
+      const activeRoom = state.activeRoom || rooms[0]; // pick last used room, or the first one
+      commit('setActiveRoom', {
+        id: activeRoom.id,
+        name: activeRoom.name
+      });
+      await chatkit.subscribeToRoom(activeRoom.id);
+
+      return true;
+      
     } catch (error) {
       handleError(commit, error)
     } finally {
